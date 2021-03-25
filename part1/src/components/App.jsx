0dfd778ba4e365141fc353c10c727f4118fd7839
MasReed <bluekitdrummer@gmail.com>
2021-03-25 09:33:12 -0500

    Simplified React App

---
+++ part1/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    const name = 'React App';
+
+    return (
+        <>
+            <h1>Hello World</h1>
+        </>
+    )
+}
+export default App
