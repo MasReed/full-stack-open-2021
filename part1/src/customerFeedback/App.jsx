1b0f39b3d661b115c2b70ad19c6477f7c06d3153
MasReed <bluekitdrummer@gmail.com>
2021-03-26 12:06:40 -0500

    Refactor handleClick events

--- part1/src/customerFeedback/App.jsx
+++ part1/src/customerFeedback/App.jsx
@@ -4,31 +4,37 @@ import Button from '../components/Buttons.jsx';
 
 const App = () => {
 
+    // Stateful Ratings
     const [ goodRating, setGoodRating ] = useState(0);
     const [ indiffRating, setIndiffRating ] = useState(0);
     const [ badRating, setBadRating ] = useState(0);
 
-    const goodClick = () => {
-        setGoodRating(goodRating + 1);
-    }
-
-    const indiffClick = () => {
-        setIndiffRating(indiffRating + 1);
-    }
-
-    const badClick = () => {
-        setBadRating(badRating + 1);
+    // Called when rating buttons are clicked
+    const handleClick = (ratingType) => {
+        switch(ratingType) {
+            case 'good':
+                setGoodRating(goodRating + 1);
+                break;
+            case 'indiff':
+                setIndiffRating(indiffRating + 1);
+                break;
+            case 'bad':
+                setBadRating(badRating + 1);
+                break;
+            default:
+                console.log('Feedback HandleClick Error');
+        }
     }
 
     // Elements to render
     return (
         <div style={{padding: "2% 25%"}}>
             <hr />
-            <h1>CUSTOMER FEEDBACK</h1>
+            <h1>Customer Feedback</h1>
             <p>Please leave your feedback by clicking a button below</p>
-            <Button handleClick={goodClick} text={'Good'}/>
-            <Button handleClick={indiffClick} text={'Indifferent'}/>
-            <Button handleClick={badClick} text={'Bad'}/>
+            <Button handleClick={() => handleClick('good')} text={'Good'}/>
+            <Button handleClick={() => handleClick('indiff')} text={'Indifferent'}/>
+            <Button handleClick={() => handleClick('bad')} text={'Bad'}/>
             <h2>Statistics</h2>
             <p>Good: {goodRating}</p>
             <p>Indifferent: {indiffRating}</p>
