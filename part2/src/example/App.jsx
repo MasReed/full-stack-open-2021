0793241998c56f0906b437a229afe4fc5b8c5e5f
MasReed <bluekitdrummer@gmail.com>
2021-03-31 15:05:21 -0500

    implement json-server for dev and useEffect hook to fetch data

--- part2/src/example/App.jsx
+++ part2/src/example/App.jsx
@@ -1,35 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Note from './Note.jsx';
 
 // Main App
 const App = () => {
 
-    // Temp Data
-    const tempNotes = [
-      {
-        id: 1,
-        content: 'HTML is easy',
-        date: '2019-05-30T17:30:31.098Z',
-        important: false
-      },
-      {
-        id: 2,
-        content: 'Browser can execute only JavaScript',
-        date: '2019-05-30T18:39:34.091Z',
-        important: true
-      },
-      {
-        id: 3,
-        content: 'GET and POST are the most important methods of HTTP protocol',
-        date: '2019-05-30T19:20:14.298Z',
-        important: true
-      }
-    ];
-
     // Stateful Data
-    const [ notes, setNotes ] = useState(tempNotes);
+    const [ notes, setNotes ] = useState([]);
     const [ newNote, setNewNote ] = useState('a new note...');
     const [ showAll, setShowAll ] = useState(true);
+
+
+    // Fetch Data from json-server
+    useEffect( () => {
+        console.log('effect');
+        axios
+            .get('http://localhost:3001/notes')
+            .then( response => {
+                console.log('promise fulfilled')
+                setNotes(response.data)
+            });
+    }, []);
 
     // User saves new note
     const addNote = (event) => {
@@ -45,7 +36,6 @@ const App = () => {
     }
 
     const handleNoteChange = (event) => {
-        console.log(event.target.value);
         setNewNote(event.target.value);
     }
 
--- part2/src/index.js
+++ part2/src/index.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// App components
 import App0 from './example/App.jsx';
 import ExercisesApp from './exercise/a/ExercisesApp.jsx';
 import PhonebookApp from './phonebook/PhonebookApp.jsx';
 
+
 // Example
-ReactDOM.render(
-  <>
-  <PhonebookApp />
-  </>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <>
+//   <PhonebookApp />
+//   </>,
+//   document.getElementById('root')
+// );
 
 // Exercise
-// ReactDOM.render(
-//     <>
-//         <App0 />
-//         <ExercisesApp />
-//     </>,
-//     document.getElementById('root')
-// );
+ReactDOM.render(
+    <>
+        <App0 />
+        <ExercisesApp />
+    </>,
+    document.getElementById('root')
+);
