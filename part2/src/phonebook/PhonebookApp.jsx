ae1d188d93856fab527b3c9c778860cec019384d
MasReed <bluekitdrummer@gmail.com>
2021-03-30 21:03:58 -0500

    phonebook app: contact name and number

--- part2/src/phonebook/PhonebookApp.jsx
+++ part2/src/phonebook/PhonebookApp.jsx
@@ -4,27 +4,47 @@ const App = () => {
 
     // Stateful list of all contacts
     const [ persons, setPersons ] = useState([
-        {name: 'Arto Hellas'},
-        {name: 'Test Person'},
-        {name: 'Cora theCat'}
+        {
+            name: 'Arto Hellas',
+            number: '867-5309'
+        },
+        {
+            name: 'Test Person',
+            number: 'TestNumber'
+        },
+        {
+            name: 'Cora theCat'
+        }
     ]);
 
-    // Stateful name of new contact
-    const [newName, setNewName ] = useState('');
+    // Stateful contact info
+    const [ newName, setNewName ] = useState('');
+    const [ newNumber, setNewNumber ] = useState('');
 
 
     // Add contact name from input
     const addContact = (event) => {
         event.preventDefault();
 
-        const contactObject = { name: newName }
-        setPersons(persons.concat(contactObject));
+        // Checks if entry alread exists
+        if (persons.some( person => person.name.toLowerCase() === newName.toLowerCase() )) {
+            alert(`${newName} is already entered.`);
+        } else {
+            const contactObject = { name: newName, number: newNumber }
+            setPersons(persons.concat(contactObject));
+        }
         setNewName('');
+        setNewNumber('');
     }
 
-    //
+    // When Name input is changed
     const handleContactChange = (event) => {
         setNewName(event.target.value);
+    }
+
+    // When Number input is changed
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value);
     }
 
     // Elements to render
@@ -33,15 +53,19 @@ const App = () => {
             <h2>Phonebook</h2>
             <form onSubmit={addContact}>
                 <div>
-                    Name: <input value={newName} onChange={handleContactChange}/>
-                    debug: {newName}
+                    <label>Name: </label>
+                    <input value={newName} onChange={handleContactChange}/>
+                </div>
+                <div>
+                    <label>Number: </label>
+                    <input value={newNumber} onChange={handleNumberChange}/>
                 </div>
                 <div>
                     <button type='submit'>Add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
-            { persons.map( person => <p key={person.name}>{person.name}</p>) }
+            { persons.map( person => <p key={person.name}>{person.name}: #{person.number}</p>) }
         </div>
     );
 }
