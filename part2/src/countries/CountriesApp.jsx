e67b67209cda463ee03076b3538c5d93fc6af75c
MasReed <bluekitdrummer@gmail.com>
2021-03-31 21:17:59 -0500

    Countries App: Display country data

--- part2/src/countries/CountriesApp.jsx
+++ part2/src/countries/CountriesApp.jsx
@@ -35,11 +35,30 @@ const App = () => {
             <label>Find Countries: </label>
             <input type='search' value={searchString} onChange={handleSearchChange}/>
             { (filteredCountries.length >= 10)
-              ? <p>Please refine your search.</p>
-              : filteredCountries.map( country => <p key={country.name}>{country.name}</p>)
+                ? <p>Please refine your search.</p>
+                : filteredCountries.map( country => <p key={country.name}>{country.name}</p>)
+            }
+            { (filteredCountries.length === 1)
+                ? filteredCountries.map( country => <DisplayCountry key={country.name} country={country} /> )
+                : null
             }
         </div>
     );
 }
 
 export default App;
+
+const DisplayCountry = ({ country }) => {
+
+    return (
+        <div>
+            <h1>{country.name}</h1>
+            <p>Capital: {country.capital}</p>
+            <p>Population: {country.population}</p>
+            <h3>Languages:</h3>
+            { country.languages.map( lang => <p key={lang.iso639_1}>{lang.name}</p>)}
+            <h3>Flag:</h3>
+            <img src={country.flag} alt='Country flag' style={{maxWidth: '300px', border: '2px solid #555'}}/>
+        </div>
+    );
+}
