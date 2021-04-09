61548ac65db82797e20ec06bd7328af1da22294a
MasReed <bluekitdrummer@gmail.com>
2021-04-09 07:05:58 -0500

    Notes App production build 1

--- part2/src/example/services/notes.js
+++ part2/src/example/services/notes.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001/api/notes';
+// const baseUrl = 'https://afternoon-harbor-27708.herokuapp.com/api/notes';
+const baseUrl = '/api/notes'
 
 const getAll = () => {
     const request = axios.get(baseUrl);
