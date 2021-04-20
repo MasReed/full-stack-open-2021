f4c35a7ee32562405fe70695d455d29701879bca
MasReed <bluekitdrummer@gmail.com>
2021-04-20 17:51:04 -0500

    add put and delete methods

--- part4/blog-list-app/controllers/blogs.js
+++ part4/blog-list-app/controllers/blogs.js
@@ -24,8 +24,27 @@ blogsRouter.post('/', async (request, response) => {
         const savedBlog = await blog.save()
         response.json(savedBlog.toJSON())
     }
+})
 
+blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+})
 
+blogsRouter.put('/:id', (request, response, next) => {
+    const body = request.body
+
+    const blog = {
+        title: body.title,
+        author: body.author,
+        date: new Date()
+    }
+
+    Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+        .then(updatedBlog => {
+            response.json(updatedBlog.toJSON())
+        })
+        .catch(error => next(error))
 })
 
 module.exports = blogsRouter
