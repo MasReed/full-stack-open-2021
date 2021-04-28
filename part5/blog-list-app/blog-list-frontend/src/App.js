1e6ff0295d3d6da815b68a09c45ad1d50f892345
MasReed <bluekitdrummer@gmail.com>
2021-04-28 15:42:42 -0500

    persist login token to browser

--- part5/blog-list-app/blog-list-frontend/src/App.js
+++ part5/blog-list-app/blog-list-frontend/src/App.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -14,11 +16,41 @@ const App = () => {
     )
   }, [])
 
+  useEffect( () => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+        const user = JSON.parse(loggedUserJSON)
+        setUser(user)
+        blogService.setToken(user.token)
+    }
+  }, [])
 
-  const handleLogin = (event) => {
-      event.preventDefault()
 
-      console.log(event.target.value)
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log(exception)
+    }
+      console.log('logging in with', username, password)
+  }
+
+  const handleLogout = async (event) => {
+    event.preventDefault()
+
+    setUser(null)
+    window.localStorage.removeItem('loggedBlogappUser')
   }
 
 
@@ -28,19 +60,24 @@ const App = () => {
 
       <hr />
 
-      <LoginForm
+      {user === null ? <LoginForm
         username={username}
         password={password}
         setUsername={setUsername}
         setPassword={setPassword}
-        onClick={handleLogin}
+        handleLogin={handleLogin}
       />
+      : <div>
+        <p>{user.username} is logged in.</p>
+        <button onClick={handleLogout}>Logout</button>
+        <hr />
 
-      <hr />
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>}
 
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+
 
     </div>
   )
