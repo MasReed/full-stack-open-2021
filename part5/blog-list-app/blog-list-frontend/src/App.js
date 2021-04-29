1a592171a4829c44509006ef6f4730b423436979
MasReed <bluekitdrummer@gmail.com>
2021-04-29 12:24:45 -0500

    implement error notifications for user

--- part5/blog-list-app/blog-list-frontend/src/App.js
+++ part5/blog-list-app/blog-list-frontend/src/App.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import LoginForm from './components/LoginForm'
 import NewBlogForm from './components/NewBlogForm'
+import Notification from './components/Notification'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [notificationMessage, setNotificationMessage] = useState('')
+  const [notificationColor, setNotificationColor] = useState('darkgrey')
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
@@ -44,8 +47,27 @@ const App = () => {
       setUser(user)
       setUsername('')
       setPassword('')
+
+      // notification banner
+      setNotificationColor('green')
+      setNotificationMessage(
+        `${username} successfully logged in!`
+      )
+      setTimeout( () => {
+        setNotificationMessage(null)
+        setNotificationColor('darkgrey')
+      }, 5000)
     } catch (exception) {
       console.log(exception)
+      // notification banner
+      setNotificationColor('red')
+      setNotificationMessage(
+        `Invalid Username or Password`
+      )
+      setTimeout( () => {
+        setNotificationMessage(null)
+        setNotificationColor('darkgrey')
+      }, 5000)
     }
       console.log('logging in with', username, password)
   }
@@ -54,29 +76,64 @@ const App = () => {
     event.preventDefault()
     setUser(null)
     window.localStorage.removeItem('loggedBlogappUser')
+
+    // notification banner
+    setNotificationColor('green')
+    setNotificationMessage(
+      `${username} successfully logged out!`
+    )
+    setTimeout( () => {
+      setNotificationMessage(null)
+      setNotificationColor('darkgrey')
+    }, 5000)
   }
 
 
   const handleNewPost = async (event) => {
     event.preventDefault()
 
-    const newBlogPost = {
-      title: newBlogTitle,
-      author: newBlogAuthor,
-      url: newBlogUrl
+    try {
+      const newBlogPost = {
+        title: newBlogTitle,
+        author: newBlogAuthor,
+        url: newBlogUrl
+      }
+
+      await blogService.create(newBlogPost)
+
+      setNewBlogTitle('')
+      setNewBlogAuthor('')
+      setNewBlogUrl('')
+
+      // notification banner
+      setNotificationColor('green')
+      setNotificationMessage(
+        `A new blog '${newBlogPost.title}' by ${newBlogPost.author} successfully added!`
+      )
+      setTimeout( () => {
+        setNotificationMessage(null)
+        setNotificationColor('darkgrey')
+      }, 5000)
+    } catch (exception) {
+      console.log(exception)
+      // notification banner
+      setNotificationColor('red')
+      setNotificationMessage(
+        `An error has occured: ${exception}`
+      )
+      setTimeout( () => {
+        setNotificationMessage(null)
+        setNotificationColor('darkgrey')
+      }, 5000)
     }
-
-    await blogService.create(newBlogPost)
-
-    setNewBlogTitle('')
-    setNewBlogAuthor('')
-    setNewBlogUrl('')
   }
 
 
   return (
     <div>
       <h2 style={{margin: '40px 0'}}>Blogs</h2>
+
+      <Notification message={notificationMessage} color={notificationColor}/>
 
       <hr />
 
