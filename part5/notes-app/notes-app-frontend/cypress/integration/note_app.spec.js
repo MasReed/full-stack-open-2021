ac7fef5786aa5a9e9d3622a2b5f90e4181dc6227
MasReed <bluekitdrummer@gmail.com>
2021-05-04 10:30:46 -0500

    refactor note importance tests

--- part5/notes-app/notes-app-frontend/cypress/integration/note_app.spec.js
+++ part5/notes-app/notes-app-frontend/cypress/integration/note_app.spec.js
@@ -62,21 +62,17 @@ describe('Note App', function() {
     })
 
     //////////////////////////////////////////
-    describe('and a note exists', function() {
+    describe('and several notes exists', function() {
       beforeEach(function() {
-        cy.createNote({
-          content: 'another cypress note',
-          important: false
-        })
+        cy.createNote({ content: 'first note', important: false })
+        cy.createNote({ content: 'second note', important: false })
+        cy.createNote({ content: 'third note', important: false })
       })
 
-      it('it can be made important', function() {
-        cy.contains('another cypress note')
-          .contains('mark important')
-          .click()
-
-        cy.contains('another cypress note')
-          .contains('mark unimportant')
+      it('other of those can be made important', function() {
+        cy.contains('second note').parent().find('button').as('theButton')
+        cy.get('@theButton').click()
+        cy.get('@theButton').should('contain', 'mark unimportant')
       })
 
     })
