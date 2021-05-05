7d4b46d00af88c3371e8b0955d36dd3699caef07
MasReed <bluekitdrummer@gmail.com>
2021-05-05 13:11:44 -0500

    implement functionality to buttons

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -1,10 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { createStore } from 'redux'
+import reducer from './reducer'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+const store = createStore(reducer)
+
+const good = () => {
+  store.dispatch({
+    type: 'GOOD'
+  })
+}
+
+const ok = () => {
+  store.dispatch({
+    type: 'OK'
+  })
+}
+
+const bad = () => {
+  store.dispatch({
+    type: 'BAD'
+  })
+}
+
+const reset = () => {
+  store.dispatch({
+    type: 'ZERO'
+  })
+}
+
+function App() {
+  return (
+    <div>
+      <button onClick={good}>good</button>
+      <button onClick={ok}>neutral</button>
+      <button onClick={bad}>bad</button>
+      <button onClick={reset}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>neutral {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  );
+}
+
+const renderApp = () => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+renderApp()
+store.subscribe(renderApp)
