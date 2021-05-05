7f4b2437d44ba6f05de409fecd0fd008f8c82eb4
MasReed <bluekitdrummer@gmail.com>
2021-05-05 13:55:49 -0500

    abstract new notes into components

--- part6/redux-example/src/App.js
+++ part6/redux-example/src/App.js
@@ -1,47 +1,13 @@
 import React from 'react'
-import { createStore } from 'redux'
-
-
-const noteReducer = (state = [], action) => {
-  if (action.type === 'NEW_NOTE') {
-    return state.concat(action.data)
-  }
-  return state
-}
-
-const store = createStore(noteReducer)
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1
-  }
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-})
-
+import NewNote from './components/NewNote'
+import Notes from './components/Notes'
 
 const App = () => {
-
   return (
     <div>
       <h1>Hello from App</h1>
-      <ul>
-        {store.getState().map(note =>
-          <li key={note.id}>
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-      </ul>
+      <NewNote />
+      <Notes />
     </div>
   )
 }
