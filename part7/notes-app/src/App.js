a225edda7d858d1bf111e05d86cf2a1ef296ee22
MasReed <bluekitdrummer@gmail.com>
2021-05-11 10:41:46 -0500

    react-router example

---
+++ part7/notes-app/src/App.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch, Route, Link
+} from 'react-router-dom'
+
+
+const Home = () => (
+  <div> <h2>TKTL notes app</h2> </div>
+)
+
+const Notes = () => (
+  <div> <h2>Notes</h2> </div>
+)
+
+const Users = () => (
+  <div> <h2>Users</h2> </div>
+)
+
+
+const App = () => {
+
+  const padding = {
+    padding: 5
+  }
+
+  return (
+    <Router>
+      <div>
+        <Link style={padding} to='/'>Home</Link>
+        <Link style={padding} to='/notes'>Notes</Link>
+        <Link style={padding} to='/users'>Users</Link>
+      </div>
+
+      <Switch>
+        <Route path='/notes'>
+          <Notes />
+        </Route>
+
+        <Route path='/users'>
+          <Users />
+        </Route>
+
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
+
+      <div>
+        <i>Note app, CR 2021</i>
+      </div>
+    </Router>
+  )
+}
+
+export default App;
