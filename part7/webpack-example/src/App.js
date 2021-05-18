d0d8ec401752d68e10e142edbb2f2acf3a553aa3
MasReed <bluekitdrummer@gmail.com>
2021-05-18 15:17:14 -0500

    configure development/production backends for webpack

--- part7/webpack-example/src/App.js
+++ part7/webpack-example/src/App.js
@@ -1,9 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './index.css'
+
+const useNotes = (url) => {
+  const [notes, setNotes] = useState([])
+  useEffect(() => {
+    axios.get(url).then(response => {
+      setNotes(response.data)
+    })
+  }, [url])
+  return notes
+}
 
 const App = () => {
   const [counter, setCounter] = useState(0)
   const [values, setValues] = useState([])
+  const notes = useNotes(BACKEND_URL)
 
   const handleClick = () => {
     setCounter(counter + 1)
@@ -15,6 +27,7 @@ const App = () => {
       Hello Webpack
       <p>{counter} clicks</p>
       <button onClick={ handleClick }>Press</button>
+      <div>{notes.length} notes are on the server {BACKEND_URL}</div>
     </div>
   )
 }
