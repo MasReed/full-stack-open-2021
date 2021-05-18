dd572f9778b04672ec8474bda421f63bf9d26ced
MasReed <bluekitdrummer@gmail.com>
2021-05-18 14:54:29 -0500

    implement source-map

--- part7/webpack-example/src/App.js
+++ part7/webpack-example/src/App.js
@@ -3,12 +3,18 @@ import './index.css'
 
 const App = () => {
   const [counter, setCounter] = useState(0)
+  const [values, setValues] = useState([])
+
+  const handleClick = () => {
+    setCounter(counter + 1)
+    setValues(values.concat(counter))
+  }
 
   return (
     <div className='container'>
       Hello Webpack
       <p>{counter} clicks</p>
-      <button onClick={ () => setCounter(counter + 1)}>Press</button>
+      <button onClick={ handleClick }>Press</button>
     </div>
   )
 }
