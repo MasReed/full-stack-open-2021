50aee3b7ac85427085198fa4ab807ad844464d69
MasReed <bluekitdrummer@gmail.com>
2021-05-21 15:17:48 -0500

    update Userlist with links to user page

--- part7/blog-list-app/blog-list-frontend/src/components/UserList.js
+++ part7/blog-list-app/blog-list-frontend/src/components/UserList.js
@@ -1,18 +1,38 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const UserList = () => {
 
   const users = useSelector( state => state.users )
 
+  const userStyle={
+    border: 'solid',
+    borderColor: 'purple',
+    borderWidth: 1,
+    marginBottom: 5,
+    padding: '10px 0 7px 4px'
+  }
+
   return (
     <div>
-      <h2>UserList</h2>
-      {
-        users.map( user => (
-          <p key={user.id}>{user.username}</p>
-        ))
-      }
+      <h2>Users</h2>
+      <h4>Blogs Created</h4>
+      <div>
+        {
+          users.map( user =>
+            <div key={user.id} style={userStyle}>
+              <p style={{ display: 'inline-block', margin: '0 20px' }}>{user.blogs.length}</p>
+              <Link
+                to={`/users/${user.id}`}
+                style={{ display: 'inline-block', marginLeft: '50px' }}
+              >
+                {user.username}
+              </Link>
+            </div>
+          )
+        }
+      </div>
     </div>
   )
 }
