a08a5b4a82c0863749cf94bf2810e9183056c4b9
MasReed <bluekitdrummer@gmail.com>
2021-05-24 11:49:13 -0500

    implement comment form and input

--- part7/blog-list-app/blog-list-frontend/src/components/BlogPage.js
+++ part7/blog-list-app/blog-list-frontend/src/components/BlogPage.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+// import blogService from '../services/blogs'
 
 const BlogPage = ({ blog }) => {
 
+  const [ comment, setComment ] = useState('')
+
   console.log('BLOG', blog)
+
+  const handleNewComment = async (event) => {
+    event.preventDefault()
+    console.log('comment:', comment)
+    setComment('')
+    return
+  }
 
   return (
     <div>
@@ -13,12 +23,23 @@ const BlogPage = ({ blog }) => {
       <p>Liked {blog.likes} times</p>
       <p>Posted by: {blog.user.username}</p>
       <hr />
-      <h3>Comments:</h3>
-      {
-        blog.comments.map( comment => (
-          <li key={comment._id}>{comment.body}</li>
-        ))
-      }
+      <div>
+        <h3>Comments:</h3>
+        <form style={{ padding: '10px' }}>
+          <input
+            id='newComment'
+            type='text'
+            value={comment}
+            onChange={ ({ target }) => setComment(target.value) }
+          />
+          <button onClick={handleNewComment}>Add Comment</button>
+        </form>
+        {
+          blog.comments.map( comment => (
+            <li key={comment._id} style={{ padding: '2px 0' }}>{comment.body}</li>
+          ))
+        }
+      </div>
     </div>
   )
 }
